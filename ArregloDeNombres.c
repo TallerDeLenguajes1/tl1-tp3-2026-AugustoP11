c3c2409ab1d8d6d13907dc29b79922e047fc9d79
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-04-16 13:20:18 -0300

    funcion BuscarNombre por palabra clave

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -9,12 +9,14 @@
 //___________________________
 //constantes
 //___________________________
-#define N 5
+#define N 2
 
 //___________________________
 //DECLARACION DE FUNCIONES
 //___________________________
 void MostrarPersonas(char *Nombres[]);
+
+int BuscarNombre(char *Nombres[], char *palabra_clave);
 
 //___________________________
 //funcion main
@@ -36,6 +38,13 @@ int main(){
     
     MostrarPersonas(V);
     
+    printf("\nIngrese una palabra clave: ");
+    gets(Buff);
+    fflush(stdin);
+    char *palabra_clave;
+    palabra_clave = (char*)malloc(strlen(Buff)*sizeof(char));
+    strcpy(palabra_clave, Buff);
+    int buscar = BuscarNombre(V, palabra_clave);
     return 0;
 }
 
@@ -47,5 +56,28 @@ void MostrarPersonas(char *Nombres[])
     for (int i = 0; i < N; i++)
     {
         printf("\nNombre %d: %s", (i+1), Nombres[i]);
+    }
+}
+
+int BuscarNombre(char *Nombres[], char *palabra_clave)
+{
+    int flag = 0;
+    int i = 0;
+    while (flag == 0 && i < N)
+    {
+        char *coincidencia = strstr(Nombres[i], palabra_clave);
+        
+        if (coincidencia)
+        {
+            printf("\nNombre: %s", Nombres[i]);
+            flag = 1;
+        }
+        i++;
+    }
+    if (flag == 1)
+    {
+        return 0;
+    } else {
+        return (-1);
     }
 }
