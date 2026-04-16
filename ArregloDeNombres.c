8284fc66fb672107451332daf254e1429bc32251
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-04-16 10:24:58 -0300

    funcion mostrarPersonas

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,10 +1,24 @@
+//___________________________
+//BIBLIOTECAS
+//___________________________
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
 
+//___________________________
+//constantes
+//___________________________
 #define N 5
 
+//___________________________
+//DECLARACION DE FUNCIONES
+//___________________________
+void mostrarPersonas(char *Nombres[]);
+
+//___________________________
+//funcion main
+//___________________________
 int main(){
 
     int i;
@@ -17,10 +31,21 @@ int main(){
         fflush(stdin);
         int longitud = strlen(Buff);
         V[i] = (char*)malloc((sizeof(char) * longitud + 1));
-        V[i] = strcpy(V[i], Buff);
+        strcpy(V[i], Buff);
     }
     
-
+    mostrarPersonas(V);
     
     return 0;
+}
+
+//___________________________
+//DEFINICION DE FUNCIONES
+//___________________________
+void mostrarPersonas(char *Nombres[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("\nNombre %d: %s", (i+1), Nombres[i]);
+    }
 }
