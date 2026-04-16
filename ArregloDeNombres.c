f855ae7979601b228ee2950f3259b37d6baa0b8d
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-04-16 10:57:04 -0300

    funcion BuscarNombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -16,6 +16,8 @@
 //___________________________
 void MostrarPersonas(char *Nombres[]);
 
+void BuscarNombre(char *Nombres[], int id);
+
 //___________________________
 //funcion main
 //___________________________
@@ -36,6 +38,13 @@ int main(){
     
     MostrarPersonas(V);
     
+    int id;
+    printf("\nIngrese un ID: ");
+    scanf("%d", &id);
+    getchar();
+
+    BuscarNombre(V, id);
+
     return 0;
 }
 
@@ -47,5 +56,15 @@ void MostrarPersonas(char *Nombres[])
     for (int i = 0; i < N; i++)
     {
         printf("\nNombre %d: %s", (i+1), Nombres[i]);
+    }
+}
+
+void BuscarNombre(char *Nombres[], int id)
+{
+    if (id > N || N < 0)
+    {
+        printf("\nNo se encontro el valor buscado.\n");
+    } else {
+        printf("\nNombre: %s\n", Nombres[id]);
     }
 }
