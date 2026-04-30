ccec407fb955ff13337d060276fc5a2b0530a340
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-04-30 07:20:04 -0300

    guardado

--- tp3_3.c
+++ tp3_3.c
@@ -6,8 +6,10 @@
 #include<stdlib.h>
 
 //_________________________
-//ARREGLO GLOBAL
+//DATOS GLOBALES
 //_________________________
+#define CANTIDAD_CLIENTES 5;
+#define CANTIDAD_PRODUCTOS 10;
 char * TiposProductos[] = {"Galletas", "Snack", "Cigarrillos", "Caramelos", "Bebidas"};
 
 //_________________________
@@ -34,6 +36,25 @@ struct Cliente {
 //_________________________
 int main() {
 
+    //Interfaz donde solicito la cantidad de clientes
+    int cantidadDeClientes;
+    do
+    {
+        printf("\nIngrese la cantidad de clientes (entre 1 y 5): ");
+        scanf("%d", &cantidadDeClientes);
+        fflush(stdin);
+    } while (cantidadDeClientes < 1 || cantidadDeClientes > 5);
+    
+    //Creo los clientes dinamicamente
+    Cliente * Clientes = (Cliente *) malloc(cantidadDeClientes * sizeof(Cliente));
+
+    //Solicito a los usuarios la carga de los clientes
+    for (int i = 0; i < (cantidadDeClientes - 1); i++)
+    {
+        (Clientes + i)->ClienteID = i;
+        printf("\nIngrese ");
+    }
+    
 
     return 0;
 }
